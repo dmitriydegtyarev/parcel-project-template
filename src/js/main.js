8e17040cbd227637ee3a2f096b1940fc9fb9545c
dmitriydegtyarev <61326282+dmitriydegtyarev@users.noreply.github.com>
2020-12-22 23:38:39 +0200

    finish slider

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+    slidesToShow:3,
+    slidesToScroll:3,
+    adaptiveHeight:true,
+
+		// responsive:[
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow:2
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 550,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	}
+		// ]
+  });
+});
